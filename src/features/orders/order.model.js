6c30ec51015dc3658898382ab0a3f77e05e869da
Diana Bobrova <diana.bobrova.spb@gmail.com>
2026-03-22 17:06:29 +0200

    add order schema

---
+++ src/features/orders/order.model.js
@@ -0,0 +1,92 @@
+const mongoose = require("mongoose");
+const { AddressSchema } = require("../users/user.model");
+
+const OrderItemSchema = new mongoose.Schema(
+  {
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    image: {
+      type: String,
+    },
+  },
+  { _id: false },
+);
+
+const OrderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    items: {
+      type: [OrderItemSchema],
+      required: true,
+    },
+    shippingAddress: {
+      type: AddressSchema,
+      required: true,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    shippingCost: {
+      type: Number,
+      default: 0,
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+      enum: ["credit", "paypal", "simulated"],
+    },
+    paymentStatus: {
+      type: String,
+      enum: ["pending", "paid", "failed"],
+      default: "pending",
+    },
+    orderStatus: {
+      type: String,
+      enum: ["pending", "processing", "shipped", "delivered", "cancelled"],
+      default: "pending",
+    },
+    trackingNumber: {
+      type: String,
+    },
+    notes: {
+      type: String,
+    },
+
+    createdAt: {
+      type: Date,
+      default: null,
+    },
+    updatedAt: {
+      type: Date,
+      default: null,
+    },
+  },
+  { timestamps: true },
+);
+
+OrderSchema.index({ user: 1 });
+OrderSchema.index({ orderStatus: 1 });
+
+const Order = mongoose.model("Order", OrderSchema);
+module.exports = Order;
