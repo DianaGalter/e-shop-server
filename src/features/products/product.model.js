7cfd3efccdc891e340600886481890a2d902d22a
Diana Bobrova <diana.bobrova.spb@gmail.com>
2026-03-22 17:04:28 +0200

    add product model

---
+++ src/features/products/product.model.js
@@ -0,0 +1,86 @@
+const mongoose = require("mongoose");
+
+const RatingSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    comment: {
+      type: String,
+    },
+  },
+  { _id: true },
+);
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: ["electronics", "clothing", "food"],
+    },
+    images: {
+      type: [String],
+      default: [],
+    },
+    stock: {
+      type: Number,
+      required: true,
+      min: 0,
+      default: 0,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    ratings: {
+      type: [RatingSchema],
+      default: [],
+    },
+    averageRating: {
+      type: Number,
+      default: 0,
+      max: 5,
+    },
+    createdAt: {
+      type: Date,
+      default: null,
+    },
+    updatedAt: {
+      type: Date,
+      default: null,
+    },
+  },
+  { timestamps: true },
+);
+
+ProductSchema.index({ category: 1 });
+ProductSchema.index({ category: 1, price: 1 });
+
+const Product = mongoose.model("Product", ProductSchema);
+module.exports = Product;
